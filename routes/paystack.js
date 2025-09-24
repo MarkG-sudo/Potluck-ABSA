428876c657d614f03e4a5945f4e85a449897170f
MarkG-sudo <gidodoom@gmail.com>
2025-09-24 13:35:08 +0000

    route update

--- routes/paystack.js
+++ routes/paystack.js
@@ -8,7 +8,7 @@ import { hasPermission, isAuthenticated } from '../middlewares/auth.js';
 const paystackRouter = Router();
 
 // ✅ Route 1: For your PWA to initiate a payment
-paystackRouter.post("/create-payment", createPaymentController);
+paystackRouter.post("/create-payment", isAuthenticated, createPaymentController);
 
 // ✅ Route 3: For Paystack to send you webhook events (must be raw body!)
 paystackRouter.post(
